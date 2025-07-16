cb14d0fe5bb2c24f641e9a20f56fc6838d536c62
Eirik <eirik.karlsen512@gmail.com>
2025-07-16 11:05:34 -0700

    refactor: fixed width and height in invoiceItem Co-authored-by: passion512 <tristanpatelpatel@gmail.com> Co-authored-by: Eirik-star <eirik.karlsen512@gmail.com>

--- src/components/InvoiceItem.js
+++ src/components/InvoiceItem.js
@@ -91,8 +91,8 @@ class ItemRow extends React.Component {
             id: this.props.item.id,
           }}/>
         </td>
-        <td className="text-center" style={{minWidth: '50px'}}>
-          <BiTrash onClick={this.onDelEvent.bind(this)} style={{height: '33px', width: '33px', padding: '7.5px'}} className="text-white mt-1 btn btn-danger"/>
+        <td className="text-center" style={{minWidth: '200px'}}>
+          <BiTrash onClick={this.onDelEvent.bind(this)} style={{height: '133px', width: '133px', padding: '7.5px'}} className="text-white mt-1 btn btn-danger"/>
         </td>
       </tr>
     );
